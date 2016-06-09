56792bad3992533733c691f8138ca2cdf2dbc742
Freddys Merici Espinoza <freddys@us.ibm.com>
2016-06-09 08:22:35 -0500

    tweaks

--- src/test/java/code_retreat/TicketTest.java
+++ src/test/java/code_retreat/TicketTest.java
@@ -1,6 +1,11 @@
 package code_retreat;
 
+import static org.testng.Assert.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
@@ -13,6 +18,7 @@ import org.testng.annotations.AfterSuite;
 
 public class TicketTest {
 	
+	private static final Logger log = LoggerFactory.getLogger(TicketTest.class);
 	private Ticket ticket;
 	private TicketTest GIVEN = this, THEN = this, WHEN = this, AND = this;
 	
@@ -23,67 +29,76 @@ public class TicketTest {
 		THEN.noPopCornButAddCoffee();
 	}
 	
-
 	private void noPopCornButAddCoffee() {
 		// TODO Auto-generated method stub
-		System.out.println("I WANT COFFEE: " + ticket.getCoffee() +", but NO POPCORN");
+		if(ticket.getWhenDinner().equals(true))
+			log.info("I WANT COFFEE: " + ticket.getCoffee() +", but NO POPCORN!!!");
+		else
+			fail("Menu checkout failed");
 	}
-
-
+	
 	private void heJustAte() {
 		// TODO Auto-generated method stub
-	new Ticket().setWhenDinner(true);
+		ticket.setWhenDinner(true);
 	}
-
-
+	
 	private void coffeeAddict() {
 		// TODO Auto-generated method stub
 		System.out.println(ticket.getPerson());
 	}
-
-
-@Test(enabled = false, dataProvider = "dp")
-  public void ftest(Integer n, String s) {
-  }
-  @BeforeMethod
-  public void beforeMethod() {
-  }
-
-  @AfterMethod
-  public void afterMethod() {
-  }
-
-
-  @DataProvider
-  public Object[][] dp() {
-    return new Object[][] {
-      new Object[] { 1, "a" },
-      new Object[] { 2, "b" },
-    };
-  }
-  @BeforeClass
-  public void beforeClass() {
-	  ticket = new Ticket();
-  }
-
-  @AfterClass
-  public void afterClass() {
-  }
-
-  @BeforeTest
-  public void beforeTest() {
-  }
-
-  @AfterTest
-  public void afterTest() {
-  }
-
-  @BeforeSuite
-  public void beforeSuite() {
-  }
-
-  @AfterSuite
-  public void afterSuite() {
-  }
-
+	
+	@Test(enabled = false, dataProvider = "dp")
+	public void f(Integer n, String s) {
+		
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		
+	}
+	
+	@DataProvider
+	public Object[][] dp() {
+		return new Object[][] {
+			new Object[] { 1, "a" },
+			new Object[] { 2, "b" },
+			};
+			
+	}
+	
+	@BeforeClass
+	public void beforeClass() {
+		ticket = new Ticket();
+		
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		
+	}
+	
+	@BeforeSuite
+	public void beforeSuite() {
+		
+	}
+	
+	@AfterSuite
+	public void afterSuite() {
+		
+	}
 }
